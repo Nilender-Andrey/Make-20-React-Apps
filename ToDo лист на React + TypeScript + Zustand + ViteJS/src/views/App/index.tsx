3af6076cdfd37ea796c6b36d5648377777049f59
Nilender-Andrey <Nilenderav@mail.ru>
2023-01-14 09:57:21 +0400

    feat: Компонент добавления задач

--- ToDo лист на React + TypeScript + Zustand + ViteJS/src/views/App/index.tsx
+++ ToDo лист на React + TypeScript + Zustand + ViteJS/src/views/App/index.tsx
@@ -1,5 +1,6 @@
-import React, { useEffect } from 'react';
+import React from 'react';
 import { useToDoStore } from '../../data/stores/useToDoStore';
+import { InputPlus } from '../components/InputPlus';
 
 import styles from './index.module.scss';
 
@@ -11,10 +12,20 @@ export const App: React.FC = () => {
     state.removeTask,
   ]);
 
+  const addTask = (title: string) => {
+    if (title) {
+      createTask(title);
+    }
+  };
+
+  console.log(tasks);
+
   return (
     <article className={styles.article}>
       <h1 className={styles.articleTitle}>To Do App</h1>
-      <section className={styles.articleSection}></section>
+      <section className={styles.articleSection}>
+        <InputPlus onAdd={addTask} />
+      </section>
       <section className={styles.articleSection}></section>
     </article>
   );
